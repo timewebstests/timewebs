12d63da973295a0f1c3cb5c3f6a6fce3a9b8ad66
ervuji <mangovendo@gmail.com>
2024-03-01 11:12:00 +0100

    added types

---
+++ types/index.ts
@@ -0,0 +1,17 @@
+export interface MainButtonProps {
+  href?: string;
+  addedStyles?: string;
+  description: string;
+}
+
+export interface ProductsCardProps {
+  imgName?: string;
+  title?: string;
+  description?: string;
+  buttonHref?: string;
+}
+
+export interface HeaderProps {
+  home: string;
+  products: string;
+}
